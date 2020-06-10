5667aae5bf8afc708c172e305fc7bd39bfd040c0
Emilio Puga <ea.pugaavalos@ugto.mx>
2020-06-10 17:24:19 -0500

    Quitar el session start de login

--- pag/api/login.php
+++ pag/api/login.php
@@ -1,16 +1,13 @@
 <?php
 // Login
 
-
-session_start();
-
 include_once 'conection.php';
 
 $usuario_login = $_POST['username'];
 $login_pass = $_POST['password'];
 
 // Si el usuario no
-$sql_user = 'Select * from usuarios where username=?';
+$sql_user = 'Select * from usuarios where username=? and activo=1';
 $sentencia_user = $pdo->prepare($sql_user);
 $sentencia_user->execute(array($usuario_login));
 $resultado = $sentencia_user->fetch();
