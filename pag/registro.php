ee431ab57abda1625c40e9c9a9a2ee4d66d91b85
Emilio Puga <ea.pugaavalos@ugto.mx>
2020-06-10 12:09:50 -0500

    Hacer que registro muestre errores de registro

--- pag/registro.php
+++ pag/registro.php
@@ -1,3 +1,42 @@
+<?php
+include_once 'api/conection.php';
+
+// Registrar un nuevo usuario
+if ($_POST && isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['password2'])) {
+
+	$usuario_new = $_POST['username'];
+	$email = $_POST['email'];
+	$pass_new = $_POST['password'];
+	$pass2_new = $_POST['password2'];
+
+	// Verificar que no exista el usuario
+	$sql_user = 'Select * from usuarios where username=?';
+	$sentencia_user = $pdo->prepare($sql_user);
+	$sentencia_user->execute(array($usuario_new));
+	$resultado = $sentencia_user->fetch();
+
+	if ($resultado) {
+		$errorMsg = 'El usuario ya existe, por favor utilice un nombre de usuario diferente';
+	} else {
+		// Revisar que las dos contraseñas sean iguales
+		if ($pass2_new == $pass_new) {
+			$pass_new = password_hash($pass_new, PASSWORD_DEFAULT);
+			$sql_agregar_user = 'Insert into usuarios (username, email, password, admin) VALUES (?,?,?,0)';
+
+			$sentencia_agregar_user = $pdo->prepare($sql_agregar_user);
+			$sentencia_agregar_user->execute(array($usuario_new, $email, $pass_new));
+
+			$sentencia_agregar_user = null;
+			$pdo = null;
+			header('Location: index.php');
+		} else {
+			$errorMsg = "Las contraseñas no son iguales";
+		}
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +50,7 @@
 
 <body class="bg-dark">
 	<div class="main">
-		
+
 		<div class="tarjeta p-4">
 			<div class="logo text-light pr-3">
 				<h1>eHelp</h1>
@@ -21,30 +60,37 @@
 				</p>
 			</div>
 			<div class="texto text-light">
+				<small>
+					<a href="index.php" class="text-info">< Regresar</a>
+				</small>
+				<hr>
 				<h3 class="form-group">Registrarse</h3>
-				<form action="/api/register.php" method="post" target="myIframe">
+				<form action="" method="post">
+					<?PHP
+					if (isset($errorMsg) && $errorMsg) {
+						echo "<p style=\"color: red;\">*", htmlspecialchars($errorMsg), "</p>\n\n";
+					}
+					?>
 					<div class="form-group">
 						Nombre de usuario:
-						<input type="text" name="username" id="" class="form-control">
+						<input minlength="3" type="text" name="username" id="" class="form-control" value="<?PHP if (isset($_POST['username'])) echo htmlspecialchars($_POST['username']); ?>">
 					</div>
 					<div class="form-group">
 						Correo electrónico:
-						<input type="email" name="email" id="" class="form-control">
+						<input minlength="3" type="email" name="email" id="" class="form-control" value="<?PHP if (isset($_POST['email'])) echo htmlspecialchars($_POST['email']); ?>">
 					</div>
 					<div class="form-group row">
 						<div class="col">
 							Contraseña:
-							<input type="password" name="password" id="" class="form-control">
+							<input minlength="3" type="password" name="password" id="" class="form-control" value="<?PHP if (isset($_POST['password'])) echo htmlspecialchars($_POST['password']); ?>">
 						</div>
 						<div class="col">
 							Confirmar contraseña:
-							<input type="password" name="password2" id="" class="form-control">
+							<input minlength="3" type="password" name="password2" id="" class="form-control" value="<?PHP if (isset($_POST['password2'])) echo htmlspecialchars($_POST['password2']); ?>">
 						</div>
 					</div>
 					<br>
-					<button type="submit" class="btn btn-primary">
-						Registrarse
-					</button>
+					<input type="submit" value="Registrarse" class="btn btn-primary">
 				</form>
 			</div>
 		</div>
