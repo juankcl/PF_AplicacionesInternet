80ac78f6102e53d5d8807312df69ee4c94f7f753
juankcl <esjuankarlos1@gmail.com>
2020-06-10 20:41:51 -0500

    Estetica dashboard

--- pag/dash.php
+++ pag/dash.php
@@ -27,67 +27,122 @@ if ($_GET) {
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <!-- Font Awesome -->
   <script src="https://kit.fontawesome.com/199cd2948c.js" crossorigin="anonymous"></script>
-
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
   <title>Dashboard</title>
 </head>
 
-<body>
-  <h1 style="text-align:center,">Dashboard de Usuarios</h1>
+<body style="background-color: rgb(21, 35, 49); color: white; font-family: 'Roboto', sans-serif;">
+
+  <div class="row" style="background-color:rgb(7, 7, 15); padding-top: 10px; padding-bottom: 8px;">
+    <div class="col">
+      <h2 style="text-align:center,font">Dashboard de Usuarios</h2>
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      <form action="api/logout.php" method="post" style="padding-right: 10px;">
+        <button style="float:inline-end; " class="btn btn-primary type=" submit">Cerrar
+          sesión</button>
+      </form>
+    </div>
+  </div>
+
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-5">
-        <form action="api/logout.php" method="post">
-          <button type="submit">Cerrar sesión</button>
-        </form>
+        <!-- Agregar USUARIO -->
         <?php if (!$_GET) : ?>
-          <h2>Agregar Usuario</h2>
-          <form method="POST" action="api/register.php">
-            <input type="text" class="form-control-mt-3" name="nombre">
-            <input type="text" class="form-control-mt-3" name="username">
-            <input type="text" class="form-control-mt-3" name="email">
-            <input type="text" class="form-control-mt-3" name="password">
-            <input type="text" class="form-control-mt-3" name="password2">
-            <button class="btn btn-primary mt-3">Agregar</button>
-          </form>
+        <h2>Agregar Usuario</h2>
+        <form class="form-group" method="POST" action="api/register.php">
+          <!-- Nombre -->
+          <div class="form-group">
+            <label>Nombre</label>
+            <input type="text" class="form-control" id="nombre" placeholder="Ingresa nombre" name="nombre">
+          </div>
+          <!-- Usuario -->
+          <div class="form-group">
+            <label>Username</label>
+            <input type="text" class="form-control" placeholder="Ingresa nombre de usuario" name="username">
+          </div>
+          <!-- Correo -->
+          <div class="form-group">
+            <label name="correo">Direccion de Correo</label>
+            <input name="correo" class="form-control" placeholder="Ingresa correo">
+          </div>
+          <!-- Contraseña -->
+          <div class="form-group">
+            <label>Contraseña</label>
+            <input type="password" class="form-control" name="password" placeholder="Ingresa Contraseña">
+          </div>
+          <!-- Contraseña confirm -->
+          <div class="form-group">
+            <label>Confirma Contraseña</label>
+            <input type="password" class="form-control" name="password2" placeholder="Ingresa Contraseña">
+            <small id="emailHelp" class="form-text text-muted">No compartas tu contraseña con nadie más</small>
+          </div>
+
+          <button type="submit" class="btn btn-primary">Agregar</button>
+        </form>
+        <!-- FIN -->
+
         <?php endif ?>
         <!-- editar form -->
         <?php if ($_GET) : ?>
-          <h2>Editar Usuario</h2>
-          <form method="POST" action="api/actualizar.php">
-            <input type="text" class="form-control-mt-3" name="nombre" value="<?php echo $resultado_unico['nombre'] ?>">
-            <input type="text" class="form-control-mt-3" name="username" value="<?php echo $resultado_unico['username'] ?>">
-            <input type="text" class="form-control-mt-3" name="email" value="<?php echo $resultado_unico['email'] ?>">
-            <input type="hidden" name="id" value="<?php echo $resultado_unico['id'] ?>">
-            <button class="btn btn-danger mt-3">Editar</button>
-          </form>
+        <h2>Editar Usuario</h2>
+        <form method="POST" action="api/actualizar.php">
+          <!-- Nombre -->
+          <div>
+            <label>Nombre</label>
+            <input type="text" class="form-control" placeholder="Ingresa nombre" name="nombre">
+          </div>
+          <!-- Usuario -->
+          <div>
+            <label>Username</label>
+            <input type="text" class="form-control" placeholder="Ingresa nombre de usuario" name="username">
+          </div>
+          <!-- Correo -->
+          <div>
+            <label>Direccion de Correo</label>
+            <input name="email" class="form-control" placeholder="Ingresa correo">
+          </div>
+          <!-- Contraseña -->
+          <div>
+            <label>Contraseña</label>
+            <input type="password" class="form-control" name="password" placeholder="Ingresa Contraseña">
+          </div>
+          <input type="hidden" name="id" value="<?php echo $resultado_unico['id'] ?>"> <br>
+          <button type="submit" class="btn btn-danger">Editar</button>
+        </form>
         <?php endif ?>
       </div>
       <div class="col-md-7">
         <?php foreach ($resultado as $dato) : ?>
-          <div class="alert alert-danger">
-            <?php echo $dato['id'] ?>
-            -
-            <?php echo $dato['nombre'] ?>
-            -
-            <?php echo $dato['username'] ?>
-            -
-            <?php echo $dato['email'] ?>
-            <form action="api/eliminar.php" method="POST" class="float-right">
-              <button type="submit" class="btn btn-link">
-                <i class="fas fa-trash"></i>
-              </button>
-              <input type="hidden" name="id" value="<?php echo $dato['id'] ?>">
-            </form>
-            <form action="" method="GET" class="float-right ml-3">
-              <button type="submit" class="btn btn-link">
-                <i class="fas fa-edit"></i>
-              </button>
-              <input type="hidden" name="id" value="<?php echo $dato['id'] ?>">
-            </form>
-          </div>
+        <div class="alert alert-danger">
+          <?php echo $dato['id'] ?>
+          -
+          <?php echo $dato['nombre'] ?>
+          -
+          <?php echo $dato['username'] ?>
+          -
+          <?php echo $dato['email'] ?>
+          <form action="api/eliminar.php" method="POST" class="float-right">
+            <button type="submit" class="btn btn-link">
+              <i class="fas fa-trash"></i>
+            </button>
+            <input type="hidden" name="id" value="<?php echo $dato['id'] ?>">
+          </form>
+          <form action="" method="GET" class="float-right ml-3">
+            <button type="submit" class="btn btn-link">
+              <i class="fas fa-edit"></i>
+            </button>
+            <input type="hidden" name="id" value="<?php echo $dato['id'] ?>">
+          </form>
+        </div>
         <?php endforeach ?>
       </div>
     </div>
